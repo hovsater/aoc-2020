db6ac5d673a93995abcb3553ccf6f0b0329c005c
Kevin Sjöberg <kevin@pushdev.se>
2020-12-06 08:53:57 +0100

    Use read instead of read_lines
    Simply to avoid (l - [""]) in part 2.

--- 06/day06.cr
+++ 06/day06.cr
@@ -1,2 +1,2 @@
-puts File.read_lines("./06/input.txt").slice_before("").sum(&.join.chars.uniq.size)
-puts File.read_lines("./06/input.txt").slice_before("").sum { |l| (l - [""]).map(&.chars).reduce { |a, b| a & b }.size }
+puts File.read("./06/input.txt").split("\n\n").sum(&.split("\n").join.chars.uniq.size)
+puts File.read("./06/input.txt").split("\n\n").sum { |l| l.split("\n").map(&.chars).reduce { |a, b| a & b }.size }
