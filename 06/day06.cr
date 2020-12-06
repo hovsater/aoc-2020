9084e02275c6bd4828ff128b510a6d877c69ad95
Kevin Sjöberg <kevin@pushdev.se>
2020-12-06 08:13:04 +0100

    Improve part 1 of day 6

--- 06/day06.cr
+++ 06/day06.cr
@@ -1,2 +1,2 @@
-puts File.read_lines("./06/input.txt").slice_before("").sum(&.join.chars.tally.keys.size)
+puts File.read_lines("./06/input.txt").slice_before("").sum(&.join.chars.uniq.size)
 puts File.read_lines("./06/input.txt").slice_before("").sum { |l| l.join.chars.tally.values.count(l[0] == "" ? l.size - 1 : l) }
