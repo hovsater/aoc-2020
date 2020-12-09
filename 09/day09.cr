0df1f9110aa59fc94f5d4a4b8ba46a9557fc3343
Kevin Sjöberg <kevin@pushdev.se>
2020-12-09 08:14:25 +0100

    Complete day 9

---
+++ 09/day09.cr
@@ -0,0 +1,23 @@
+def part1(input, preamble)
+  input.map(&.to_u64).reverse_each.each_cons(preamble + 1, reuse: true) do |a|
+    a[1..].combinations(2).map(&.sum).includes?(a[0]) || return a[0]
+  end
+end
+
+def part2(input, preamble)
+  target = part1(input, preamble) || 0
+
+  input.each_index do |i|
+    sum = 0
+    nums = input[i..].take_while do |n|
+      sum += n
+      sum < target
+    end
+
+    return nums.minmax.sum if sum == target
+  end
+end
+
+input = File.read_lines("./09/input.txt").map(&.to_u64)
+puts part1(input, 25)
+puts part2(input, 25)
