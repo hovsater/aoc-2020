4534de9c957cb8800f91ceacfe9c027c7c64fdb7
Kevin Sjöberg <kevin@pushdev.se>
2020-12-10 08:14:46 +0100

    Improve part 1 of day 10

--- 10/day10.cr
+++ 10/day10.cr
@@ -1,17 +1,9 @@
 TEST_INPUT = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
 REAL_INPUT = File.read_lines("#{__DIR__}/input.txt").map(&.to_i)
 
-def part_1
-  differences = Hash(Int32, Int32).new(0)
-  differences[3] += 1
-
-  REAL_INPUT.sort.reduce(0) do |jolts, adapter|
-    diff = adapter - jolts
-    differences[diff] += 1
-    adapter
-  end
-
-  differences.values.product
+def part_1(input)
+  sorted = input.sort
+  ([0] + sorted + [sorted.last + 3]).each_cons(2, reuse: true).map { |(a, b)| b - a }.tally.values.product
 end
 
-puts part_1
+puts part_1(REAL_INPUT)
