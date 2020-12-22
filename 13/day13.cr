72e8da894cd760a2724fe79163316eea08f89524
Kevin Sjöberg <kevin@pushdev.se>
2020-12-22 18:35:26 +0100

    Complete day 13

--- 13/day13.cr
+++ 13/day13.cr
@@ -1,10 +1,29 @@
 REAL_INPUT = File.read_lines("#{__DIR__}/real.txt")
 TEST_INPUT = File.read_lines("#{__DIR__}/test.txt")
 
-def parse(input)
+def part_1(input)
   estimate = input.first.to_i
   buss_ids = input.last.split(",").reject(&.==("x")).map(&.to_i)
-  buss_ids.map { |id| {id, id - estimate % id} }.min_by(&.[1]).product
+  puts buss_ids.map { |id| {id, id - estimate % id} }.min_by(&.[1]).product
 end
 
-puts parse(REAL_INPUT)
+def part_2(input)
+  busses = input.last.split(",")
+  time = 0_u64
+  multiplier = 1_u64
+
+  busses.each_with_index do |id, offset|
+    next if id == "x"
+
+    while (time + offset) % id.to_i != 0
+      time += multiplier
+    end
+
+    multiplier *= id.to_i
+  end
+
+  puts time
+end
+
+part_1(REAL_INPUT)
+part_2(REAL_INPUT)
