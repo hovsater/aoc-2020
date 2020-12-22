d3c7011bf68fa27172ddac6a932fda16f2ca094e
Kevin Sjöberg <kevin@pushdev.se>
2020-12-22 12:03:56 +0100

    Complete part one of day 12

---
+++ 12/day12.cr
@@ -0,0 +1,40 @@
+TEST_INPUT = File.read("#{__DIR__}/test.txt")
+REAL_INPUT = File.read("#{__DIR__}/real.txt")
+
+enum Direction
+  NORTH =   0
+  EAST  =  90
+  SOUTH = 180
+  WEST  = 270
+end
+
+def parse(instructions)
+  direction = Direction::EAST
+  x, y = 0, 0
+
+  instructions.scan(/([A-Z])(\d+)/).each do |(_, action, value)|
+    case action
+    when "N" then y += value.to_i
+    when "S" then y -= value.to_i
+    when "E" then x += value.to_i
+    when "W" then x -= value.to_i
+    when "L" then direction = Direction.new((direction.value - value.to_i + 360) % 360)
+    when "R" then direction = Direction.new((direction.value + value.to_i + 360) % 360)
+    when "F"
+      case direction
+      when Direction::NORTH then y += value.to_i
+      when Direction::SOUTH then y -= value.to_i
+      when Direction::EAST  then x += value.to_i
+      when Direction::WEST  then x -= value.to_i
+      end
+    end
+  end
+
+  x.abs + y.abs
+end
+
+def part_1
+  puts parse(REAL_INPUT)
+end
+
+part_1
