cea884065ec39830d2df3e610669abf1ec808be3
Kevin Sjöberg <kevin@pushdev.se>
2020-12-22 12:30:38 +0100

    Complete day 12

--- 12/day12.cr
+++ 12/day12.cr
@@ -10,7 +10,7 @@ end
 
 def parse(instructions)
   direction = Direction::EAST
-  x, y = 0, 0
+  x, y = {0, 0}
 
   instructions.scan(/([A-Z])(\d+)/).each do |(_, action, value)|
     case action
@@ -33,8 +33,44 @@ def parse(instructions)
   x.abs + y.abs
 end
 
+def rotate(point, degrees)
+  x, y = point
+  radians = degrees * (Math::PI / 180)
+
+  nx = (x * Math.cos(radians) - y * Math.sin(radians)).round.to_i
+  ny = (x * Math.sin(radians) + y * Math.cos(radians)).round.to_i
+
+  {nx, ny}
+end
+
+def parse_with_waypoint(instructions)
+  ox, oy = 0, 0
+  px, py = 10, 1
+
+  instructions.scan(/([A-Z])(\d+)/).each do |(_, action, value)|
+    case action
+    when "N" then py += value.to_i
+    when "S" then py -= value.to_i
+    when "E" then px += value.to_i
+    when "W" then px -= value.to_i
+    when "L" then px, py = rotate({px, py}, degrees: value.to_i)
+    when "R" then px, py = rotate({px, py}, degrees: -value.to_i)
+    when "F"
+      ox = ox + (px * value.to_i)
+      oy = oy + (py * value.to_i)
+    end
+  end
+
+  ox.abs + oy.abs
+end
+
 def part_1
   puts parse(REAL_INPUT)
 end
 
+def part_2
+  puts parse_with_waypoint(REAL_INPUT)
+end
+
 part_1
+part_2
